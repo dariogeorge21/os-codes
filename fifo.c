0bcfd701b7d002868f5b822cb6ac205ef6f45ff9
Dario George <edu.dariogeorge21@gmail.com>
2026-05-21 12:00:53 +0530

    Add implementation of disk scheduling algorithm

--- fifo.c
+++ fifo.c
@@ -1,60 +1,51 @@
 #include <stdio.h>
 
-int main(void)
-{
-	int frames, pages;
-	if (printf("Enter number of frames: "), fflush(stdout), scanf("%d", &frames) != 1)
-		return 0;
-	if (printf("Enter number of pages: "), fflush(stdout), scanf("%d", &pages) != 1)
-		return 0;
+int main() {
+    int pages[100], frames[10], n, f, i, j;
+    int faults = 0, index = 0, found;
 
-	if (frames <= 0 || pages <= 0)
-		return 0;
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
 
-	if (frames > 10)
-		frames = 10;
-	if (pages > 50)
-		pages = 50;
+    printf("Enter page reference string: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &pages[i]);
 
-	int frame[10], ref[50];
-	int i, j, k = 0;
-	int faults = 0, hits = 0;
+    printf("Enter number of frames: ");
+    scanf("%d", &f);
 
-	printf("Enter reference string: ");
-	for (i = 0; i < pages; i++)
-		scanf("%d", &ref[i]);
+    for(i = 0; i < f; i++)
+        frames[i] = -1;
 
-	for (i = 0; i < frames; i++)
-		frame[i] = -1;
+    for(i = 0; i < n; i++) {
+        found = 0;
 
-	for (i = 0; i < pages; i++) {
-		int found = 0;
-		for (j = 0; j < frames; j++) {
-			if (frame[j] == ref[i]) {
-				found = 1;
-				hits++;
-				break;
-			}
-		}
+        // Check if page already in frame
+        for(j = 0; j < f; j++) {
+            if(frames[j] == pages[i]) {
+                found = 1;
+                break;
+            }
+        }
 
-		if (!found) {
-			frame[k] = ref[i];
-			k = (k + 1) % frames;
-			faults++;
-		}
+        // Page fault
+        if(found == 0) {
+            frames[index] = pages[i];
+            index = (index + 1) % f;
+            faults++;
+        }
 
-		/* Print frames */
-		printf("\nFrames: ");
-		for (j = 0; j < frames; j++) {
-			if (frame[j] == -1)
-				printf("- ");
-			else
-				printf("%d ", frame[j]);
-		}
-	}
+        // Display frames
+        printf("\nPage %d: ", pages[i]);
+        for(j = 0; j < f; j++) {
+            if(frames[j] != -1)
+                printf("%d ", frames[j]);
+            else
+                printf("- ");
+        }
+    }
 
-	printf("\nPage Faults = %d\n", faults);
-	printf("Page Hits = %d\n", hits);
+    printf("\nTotal Page Faults = %d\n", faults);
 
-	return 0;
+    return 0;
 }
